4c0e8564d735b561c1147c2d4f79b74f2a4a75ba
PexMor <p.moravek@xlinux.cz>
2022-09-24 18:52:51 +0200

    add aliases

--- aliases.sh
+++ aliases.sh
@@ -1,13 +1,4 @@
-# netlab
-
-Network lab with container and netns
-
-## Useful aliases
-
-```bash
+# file to include into current shell context via 'source aliases.sh'
 alias rin='sudo ip netns exec netlab'
-# LC_ALL=C
 alias myssh='sudo ip netns exec netlab ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=ERROR'
 alias mysftp='sudo ip netns exec netlab sftp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=ERROR'
-```
-
